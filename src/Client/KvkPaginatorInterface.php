3e6c31fc37cf39c76937fb85f892842e79b3797a
Eugène Spaan <eugene.spaan@planbition.com>
2020-11-04 15:58:20 +0100

    Updated KvKPaginatorInterface

--- src/Client/KvkPaginatorInterface.php
+++ src/Client/KvkPaginatorInterface.php
@@ -7,4 +7,10 @@ interface KvkPaginatorInterface
     public function getNextUrl(): ?string;
 
     public function getPreviousUrl(): ?string;
+
+    public function getItems(): array;
+
+    public function getTotalItems(): int;
+
+    public function getItemsPerPage(): int;
 }
